3caef2a4b94d98c72fb4913f340b8b28c7c58218
papaseyone <charleskumahntim@gmail.com>
2024-03-25 13:01:28 -0700

    updated

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#make changes to confg file using puppet
+
+file { 'ect/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+	
+	#SSH client configuration
+	host*
+	identityFile ~/.ssh/school
+	passwordAuthentication no
+
+}
