f6fee5fbd3ef3635163a00b30978b285dc8f1be3
papaseyone <charleskumahntim@gmail.com>
2024-03-28 11:20:46 -0700

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
-# Install specific version of flask (2.1.0)
+# Install a specific version of flask (2.1.0)
 package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+	ensure   => '2.1.0',
+	provider => 'pip'
 }
 
